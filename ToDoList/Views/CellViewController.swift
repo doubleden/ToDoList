8e799e02c0123ad6111e137273c2972e4013d936
Denis Denisov <denis.denisov8701@gmail.com>
2024-08-27 11:21:16 +0200

    Storyboard setup

---
+++ ToDoList/Views/CellViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CellViewController.swift
+//  ToDoList
+//
+//  Created by Denis Denisov on 27/8/24.
+//
+
+import UIKit
+
+class CellViewController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
