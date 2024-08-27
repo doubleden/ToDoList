d00e05dd958184d739c67141be5db27970228cc8
Denis Denisov <denis.denisov8701@gmail.com>
2024-08-27 13:38:06 +0200

    setup services

---
+++ ToDoList/Services/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  ToDoList
+//
+//  Created by Denis Denisov on 27/8/24.
+//
+
+import Foundation
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+}
---
+++ ToDoList/Services/StorageManager.swift
@@ -0,0 +1,15 @@
+//
+//  StorageManager.swift
+//  ToDoList
+//
+//  Created by Denis Denisov on 27/8/24.
+//
+
+import CoreData
+
+final class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    private init() {}
+}
