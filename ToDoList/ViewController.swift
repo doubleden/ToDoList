add4021b6a5bbb20e1f292603124a118c2692711
Denis Denisov <denis.denisov8701@gmail.com>
2024-08-27 09:46:46 +0200

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Denis Denisov on 27/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
